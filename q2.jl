ec7e07f7ef914f78a0f1ebddc75a870e1cdcf4f4
Ilan Grynszpan <ilan.grynszpan@gmail.com>
2024-11-19 22:18:13 -0300

    optimizer added

--- q2.jl
+++ q2.jl
@@ -1,7 +1,30 @@
 ## Initial setup
 import Pkg
-Pkg.add(["Distributions", "LinearAlgebra", "Random", "StatsBase", "StatsPlots"])
+Pkg.add(["Distributions", "LinearAlgebra", "Random", "StatsBase", "StatsPlots", "JuMP"])
 Pkg.add("Plots")
-using Distributions, LinearAlgebra, Random, StatsBase, StatsPlots, Plots
+Pkg.add("Optim")
+Pkg.add(["DataFrames", "CSV"])
+using Distributions, LinearAlgebra, Random, StatsBase, StatsPlots, Plots, Optim, JuMP, DataFrames, CSV
 
 Random.seed!(1234)
+
+### Testing Gurobi
+
+model = Model(Optim.Optimizer);
+set_optimizer_attribute(model, "method", BFGS());
+@variable(model, x)
+@objective(model, Max, -(3.0*x^4.0 - 13.0*x^2.0 + 4.0*x + 12.0))
+optimize!(model)
+
+objective_value(model)
+value.(x)
+
+### Reading historical data into a DataFrame
+
+df = CSV.read("./data/input/ipeadata-consumo-energia-SE.csv", DataFrame)
+
+y = df[:,2]
+
+### Defining the model
+
+modelo = Model(Optim.Optimizer);
